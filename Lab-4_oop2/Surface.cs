165a689cd0caad99cbe3266aa4318abce1bc6c5c
drumil2407 <78135386+drumil2407@users.noreply.github.com>
2021-10-14 10:05:27 -0400

    Lab4
    Adding Sensor and material files in project

---
+++ Lab-4_oop2/Surface.cs
@@ -0,0 +1,36 @@
+﻿
+using Lab_4_oop2.Geometry2D;
+using Lab_4_oop2.Particle;
+
+namespace Lab_4_oop2.ModelComponents
+{
+    public interface ISurface
+		{
+			Cell HandlePhonon(Phonon p);
+		}
+
+		public class BoundarySurface : ISurface
+		{
+			public SurfaceLocation Location { get; }
+			protected Cell cell;
+
+			public BoundarySurface(SurfaceLocation location, Cell cell)
+			{
+				Location = location;
+				this.cell = cell;
+			}
+			public virtual Cell HandlePhonon(Phonon p)
+			{
+				Vector direction = p.Direction;
+				if (Location == SurfaceLocation.left || Location == SurfaceLocation.right)
+				{
+					p.SetDirection(-direction.DX, direction.DY);
+				}
+				else
+				{
+					p.SetDirection(direction.DX, -direction.DY);
+				}
+				return cell;
+			}
+		}
+}
